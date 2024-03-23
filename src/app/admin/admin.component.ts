e555307cad6694c1c1324d2523df5412b97f424f
kitokid <ourabisaif051@gmail.com>
2024-03-23 23:45:21 +0100

    update offer form

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { OffreserviceService } from '../services/offreservice.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms'; // Import FormBuilder and Validators
 
 @Component({
   selector: 'app-admin',
@@ -9,26 +10,45 @@ import { OffreserviceService } from '../services/offreservice.service';
 export class AdminComponent implements OnInit {
   offres: any[];
   p: number = 1;
-  constructor(private offreserviceService: OffreserviceService) { }
+  c = false;
+  updateform: FormGroup;
 
+  constructor(
+    private offreserviceService: OffreserviceService,
+    private formBuilder: FormBuilder // Inject FormBuilder here
+  ) {
+    this.updateform = this.formBuilder.group({
+      id: ['', [Validators.required, Validators.pattern(/^[a-zA-Z]+$/)]],
+      Experience: ['', [Validators.required, Validators.pattern(/^[a-zA-Z]+$/)]],
+      Salaire: [''],
+      date_exp: ['', [Validators.required, Validators.email]],
+      description: ['', [Validators.required]],
+    });
+  }
   ngOnInit(): void {
     this.offreserviceService.getOffres().subscribe(data => {
       this.offres = data;
+      console.log(this.offres);
     });
   }
 
 
-
-  deleteoffer(id){
-    this.offreserviceService.deleteOffre(id).subscribe((rep:any)=>{
-      if(rep==1){
-        this.ngOnInit()
-      }
-    })
+  enterforme(id,exp,sal,dat) {
+      this.updateform.setValue({
+        id:id,
+        Experience:exp,
+        Salaire:sal,
+        date_exp:dat,
+        description:"",
+      });
+    ;
   }
 
-
-
-
-
+  deleteoffer(id): void {
+    this.offreserviceService.deleteOffre(id).subscribe((rep: any) => {
+      if (rep == 1) {
+        this.ngOnInit();
+      }
+    });
+  }
 }
